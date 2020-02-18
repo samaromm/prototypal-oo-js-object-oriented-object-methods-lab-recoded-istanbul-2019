7ab616df641ccdc4d58ca25b0a1c5caa7c5ce42c
Samar Omar <samaromm98@gmail.com>
2020-02-18 20:16:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-function BoardMember(name, homeState, )
+function BoardMember(name, homeState,training ){
+  this.name=name
+  this.homeState=homeState
+  this.training=training
+}
+
+BoardMember.prototype.vet(){
+  return ''
+}
